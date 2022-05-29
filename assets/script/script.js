893a1afe5216883a2f41c7173120ac192292bfcd
Katrina  Shih <katrinashih@Katrinas-MacBook-Pro.local>
2022-05-29 14:11:56 -0400

    list out all the elements in the weather dashboard

--- assets/script/script.js
+++ assets/script/script.js
@@ -38,3 +38,46 @@ var getCityWeather = function(city){
         });
     });
 };
+
+var displayWeather = function(weather, searchCity){
+    //clear old content
+    weatherContainerEl.textContent= "";  
+    citySearchInputEl.textContent=searchCity;
+
+    var currentDate = document.createElement("span")
+    currentDate.textContent=" (" + moment(weather.dt.value).format("MMM D, YYYY") + ") ";
+    citySearchInputEl.appendChild(currentDate);
+ 
+    //create an image element
+    var weatherIcon = document.createElement("img")
+    weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`);
+    citySearchInputEl.appendChild(weatherIcon);
+ 
+    //create a span element to hold temperature data
+    var temperatureEl = document.createElement("span");
+    temperatureEl.textContent = "Temperature: " + weather.main.temp + " °F";
+    temperatureEl.classList = "list-group-item"
+   
+    //create a span element to hold Humidity data
+    var humidityEl = document.createElement("span");
+    humidityEl.textContent = "Humidity: " + weather.main.humidity + " %";
+    humidityEl.classList = "list-group-item"
+ 
+    //create a span element to hold Wind data
+    var windSpeedEl = document.createElement("span");
+    windSpeedEl.textContent = "Wind Speed: " + weather.wind.speed + " MPH";
+    windSpeedEl.classList = "list-group-item"   
+    
+    //append to container
+    weatherContainerEl.appendChild(temperatureEl);
+ 
+    //append to container
+    weatherContainerEl.appendChild(humidityEl);
+ 
+    //append to container
+    weatherContainerEl.appendChild(windSpeedEl);
+ 
+    var lat = weather.coord.lat;
+    var lon = weather.coord.lon;
+    getUvIndex(lat,lon)
+ }
